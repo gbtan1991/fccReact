a16bdd9f11d6b1e62ad230e1ce9596e38739f789
Gilbert Tan <gilbert.tan1991@gmail.com>
2023-09-28 21:08:20 +0800

    I'm now learning

---
+++ ReactFacts/index.js
@@ -0,0 +1,51 @@
+//1. BASIC RENDER
+//ReactDOM.render(<h1>Hello Everyone!</h1>, document.getElementById('root'));
+
+//2. RENDER ANYHTING
+ReactDOM.render(<h1>Lucky number is : {Math.floor(Math.random() * 10)}</h1>, document.getElementById('root'));
+
+//3. CREATE A LIST
+// ReactDOM.render(
+// <div>
+//         <p>This is the Shopping list</p>
+//         <ul>
+//             <li>3pcs Apples</li>
+//             <li>2pcs Banana</li>
+//             <li>4pcs Lemon</li>
+
+//         </ul>
+
+//         </div>
+
+// , document.getElementById('root'));
+
+
+//4. function Navbar() {
+    // return(
+    //     <div>
+    //     <h1>Company Name</h1>
+    //     <ul>
+    //         <li>About us</li>
+    //         <li>Projects</li>
+    //         <li>Contact us</li>
+    
+    //     </ul>
+    //     </div>
+    //     )
+    
+    // }
+    
+    
+    // ReactDOM.render(
+    
+    //     <div>
+    //         <Navbar />
+    //     </div>
+    
+    // , document.getElementById('root'));
+    
+
+
+
+
+
