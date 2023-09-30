30bdce9bea1f93843e4fe7dcde0386ab6796d567
Gilbert Tan <57788135+gbtan1991@users.noreply.github.com>
2023-09-30 11:57:16 +0800

    add files
    add files

---
+++ Project1/main.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+
+const navbar = (
+    <nav>
+        <h1>The W3bsit3</h1>
+
+        <ul>
+            <li>Pricing</li>
+            <li>About</li>
+            <li>Contact</li>
+
+        </ul>
+    </nav>
+)
+
+
+ReactDOM.render(navbar, document.getElementById('root'));
+
--- ReactFacts/index.js
+++ ReactFacts/index.js
@@ -2,7 +2,7 @@
 //ReactDOM.render(<h1>Hello Everyone!</h1>, document.getElementById('root'));
 
 //2. RENDER ANYHTING
-ReactDOM.render(<h1>Lucky number is : {Math.floor(Math.random() * 10)}</h1>, document.getElementById('root'));
+// ReactDOM.render(<h1>Lucky number is : {Math.floor(Math.random() * 10)}</h1>, document.getElementById('root'));
 
 //3. CREATE A LIST
 // ReactDOM.render(
@@ -50,6 +50,27 @@ ReactDOM.render(<h1>Lucky number is : {Math.floor(Math.random() * 10)}</h1>, doc
 // h1.textContent = 'This is an imperative way to program';
 // h1.className = 'header';
 // document.getElementById('root').appendChild(h1);
+
+//6. CREATE A NAV BAR IN JSX
+
+
+const navbar = (
+    <nav>
+        <h1>The W3bsit3</h1>
+
+        <ul>
+            <li>Pricing</li>
+            <li>About</li>
+            <li>Contact</li>
+
+        </ul>
+    </nav>
+)
+
+
+
+ReactDOM.render(navbar, document.getElementById('root'));
+
 
 
 
